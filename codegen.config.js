5fda492eb57b45ceae5ddafd096b7a1e661b9cb3
pnly <pnlybubbles@gmail.com>
2022-02-23 20:53:06 +0900

    graphql-codegenでintrospectionするのにadminのsecretが必要なので埋め込む

---
+++ codegen.config.js
@@ -0,0 +1,21 @@
+/** @type { import('@graphql-codegen/plugin-helpers').Types.Config } */
+module.exports = {
+  overwrite: true,
+  schema: {
+    'https://idolypride-spotlight.hasura.app/v1/graphql': {
+      headers: {
+        'x-hasura-admin-secret': process.env.HASURA_ADMIN_SECRET,
+        'x-hasura-role': 'user',
+      },
+    },
+  },
+  documents: 'api/**/*.graphql',
+  generates: {
+    'generated/graphql.ts': {
+      plugins: ['typescript', 'typescript-operations', 'typed-document-node'],
+    },
+    './graphql.schema.json': {
+      plugins: ['introspection'],
+    },
+  },
+}
