53d9ccee6c11f64b6ce6ea937de82d41b200de35
pnly <pnlybubbles@gmail.com>
2022-02-24 05:24:36 +0900

    mutationしてみた

--- pages/idol/new.vue
+++ pages/idol/new.vue
@@ -2,7 +2,16 @@
   <div>
     <NuxtLink to="/idol">アイドル一覧</NuxtLink>
     <h1>アイドル追加</h1>
+    <input v-model="name" type="text" />
+    <button @click="submit">追加</button>
   </div>
 </template>
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { useMutation } from '@urql/vue'
+import { CreateIdolDocument } from '~~/generated/graphql'
+
+const name = ref('')
+const { executeMutation } = useMutation(CreateIdolDocument)
+const submit = () => executeMutation({ object: { name: name.value } })
+</script>
 <style lang="scss" scoped></style>
