2877f2b74b5e978da7d2b32cacc04e376b5185a5
pnly <pnlybubbles@gmail.com>
2022-02-26 01:37:08 +0900

    mutation直後にqueryが呼び出されるように遷移してみる

--- pages/idol/new.vue
+++ pages/idol/new.vue
@@ -1,17 +1,21 @@
 <template>
   <div>
-    <NuxtLink to="/idol">アイドル一覧</NuxtLink>
     <h1>アイドル追加</h1>
-    <input v-model="name" type="text" />
-    <button @click="submit">追加</button>
+    <input v-model="name" type="text" :disabled="fetching" />
+    <button :disabled="fetching" @click="submit">追加</button>
   </div>
 </template>
 <script setup lang="ts">
 import { useMutation } from '@urql/vue'
 import { CreateIdolDocument } from '~~/generated/graphql'
 
+const { push } = useRouter()
 const name = ref('')
-const { executeMutation } = useMutation(CreateIdolDocument)
-const submit = () => executeMutation({ object: { name: name.value } })
+const { executeMutation, fetching } = useMutation(CreateIdolDocument)
+const submit = async () => {
+  await executeMutation({ object: { name: name.value } })
+  name.value = ''
+  push('/idol')
+}
 </script>
 <style lang="scss" scoped></style>
