73542636f453a8e5681cec59d7782a22483912bd
pnly <pnlybubbles@gmail.com>
2022-02-26 03:32:20 +0900

    TextFieldコンポーネントを追加

---
+++ components/text-field.vue
@@ -0,0 +1,32 @@
+<template>
+  <input
+    :model="modelValue"
+    class="text-field"
+    type="text"
+    :disabled="disabled ?? false"
+    @input="$emit('update:modelValue', ($event.target as any).value)"
+  />
+</template>
+<script setup lang="ts">
+interface Props {
+  modelValue: string
+  disabled?: boolean
+}
+interface Emits {
+  (e: 'update:modelValue', value: string): void
+}
+defineProps<Props>()
+defineEmits<Emits>()
+</script>
+<style lang="scss" scoped>
+.text-field {
+  border: 0;
+  background-color: rgba(#fff, 0.1);
+  padding: 8px;
+  border-radius: 4px;
+  font-size: 16px;
+  line-height: 1em;
+  color: #fff;
+  outline: none;
+}
+</style>
---
+++ pages/fumen/new.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <h1>譜面を追加する</h1>
+    <div class="section">
+      <div class="label">曲名</div>
+      <TextField v-model="name"></TextField>
+    </div>
+    <!-- <button :disabled="fetching" @click="submit">追加</button> -->
+  </div>
+</template>
+<script setup lang="ts">
+// import { useMutation } from '@urql/vue'
+// import { CreateIdolDocument } from '~~/generated/graphql'
+
+// const { push } = useRouter()
+const name = ref('')
+// const { executeMutation, fetching } = useMutation(CreateIdolDocument)
+// const submit = async () => {
+//   await executeMutation({ object: { name: name.value } })
+//   name.value = ''
+//   push('/idol')
+// }
+</script>
+<style lang="scss" scoped>
+.section {
+  display: grid;
+  grid: auto-flow / auto;
+  gap: 8px;
+  padding: 0 24px;
+}
+.label {
+  font-size: 12px;
+}
+</style>
