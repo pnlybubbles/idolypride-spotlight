62ca13cc367ec11c09e65262e5babcb305f0c0a4
pnly <pnlybubbles@gmail.com>
2022-02-26 22:08:17 +0900

    コンポーネント名を2ワードに強制するルールを無効化

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,5 +3,6 @@ module.exports = {
   extends: ['plugin:vue/vue3-recommended', '@vue/eslint-config-typescript/recommended', '@vue/eslint-config-prettier'],
   rules: {
     '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+    'vue/multi-word-component-names': 'off',
   },
 }
