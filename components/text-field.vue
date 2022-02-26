68fa5ab5b6bd2ab28301033ff6a530343da59ef8
pnly <pnlybubbles@gmail.com>
2022-02-27 03:50:39 +0900

    TextFieldにエラー補助テキストを表示する機能を追加

--- components/text-field.vue
+++ components/text-field.vue
@@ -1,29 +1,50 @@
 <template>
-  <input
-    :model="modelValue"
-    class="text-field"
-    type="text"
-    :placeholder="placeholder ?? ''"
-    :disabled="disabled ?? false"
-    @input="$emit('update:modelValue', ($event.target as any).value)"
-  />
+  <div class="text-field">
+    <input
+      :model="modelValue"
+      class="input"
+      :class="{ error: errorHysteresis }"
+      type="text"
+      :placeholder="placeholder ?? ''"
+      :disabled="disabled ?? false"
+      @input="$emit('update:modelValue', ($event.target as any).value)"
+      @focus="focusing = true"
+      @blur="focusing = false"
+    />
+    <div v-if="errorHysteresis" class="assistive error"><slot name="error"></slot></div>
+  </div>
 </template>
 <script setup lang="ts">
 interface Props {
   modelValue: string
   disabled?: boolean
   placeholder?: string
+  error?: boolean
 }
 interface Emits {
   (e: 'update:modelValue', value: string): void
 }
-defineProps<Props>()
+const props = defineProps<Props>()
 defineEmits<Emits>()
+const focusing = ref(false)
+const errorHysteresis = ref(false)
+watchEffect(() => {
+  if (focusing.value && props.error) {
+    return
+  }
+  errorHysteresis.value = props.error ?? false
+})
 </script>
 <style lang="scss" scoped>
 @import './token.scss';
 
 .text-field {
+  display: grid;
+  grid: auto-flow / auto;
+  gap: 2px;
+}
+
+.input {
   border: 0;
   background-color: $surface1;
   padding: 8px;
@@ -32,9 +53,23 @@ defineEmits<Emits>()
   line-height: 1em;
   color: $text1;
   outline: none;
+  border: solid 1px transparent;
 
   &::placeholder {
     color: $text3;
+  }
+
+  &.error {
+    border-color: $error;
+  }
+}
+
+.assistive {
+  padding: 0 8px;
+  font-size: 12px;
+
+  &.error {
+    color: $error;
   }
 }
 </style>
