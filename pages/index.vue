4595fcaea071fb5f94bf49b21a643881a2df2463
pnly <pnlybubbles@gmail.com>
2022-02-28 01:31:01 +0900

    サーバーから譜面一覧を取得して表示する

--- pages/index.vue
+++ pages/index.vue
@@ -8,16 +8,18 @@
   <Shell v-else-if="isAuthenticated">
     <template #heading>IDOLY PRIDE SPOTLIGHT</template>
     <div class="list">
-      <NuxtLink v-for="item in data" :key="item.id" :to="`/fumen/${item.id}`" class="item">
-        {{ item.title }}
+      <NuxtLink v-for="item in fumenList" :key="item.id" :to="`/fumen/${item.id}`" class="item">
+        <div class="title">{{ item.title }}</div>
+        <div class="unit">{{ item.unit }}</div>
       </NuxtLink>
       <ButtonLink to="/fumen/new">譜面追加</ButtonLink>
     </div>
   </Shell>
 </template>
 <script setup lang="ts">
-import data from '~/data/live'
+import { useQuery } from '@urql/vue'
 import { useAuth } from '~~/composable/auth0'
+import { GetFumentListDocument } from '~~/generated/graphql'
 
 const { isAuthenticated, user, busy, getToken, signIn } = useAuth()
 
@@ -28,6 +30,12 @@ watchEffect(async () => {
   const token = await getToken()
   console.log(token)
 })
+
+const { data, error } = useQuery({ query: GetFumentListDocument })
+if (error.value) {
+  console.error(error.value)
+}
+const fumenList = computed(() => data.value?.fumen ?? [])
 </script>
 <style lang="scss" scoped>
 @import '../components/token.scss';
@@ -45,7 +53,17 @@ watchEffect(async () => {
   background-color: $surface1;
   border-radius: 4px;
   padding: 16px;
+}
+
+.title {
   font-weight: bold;
+  font-size: 16px;
+  color: $text1;
+}
+
+.unit {
+  font-size: 14px;
+  color: $text3;
 }
 
 a {
