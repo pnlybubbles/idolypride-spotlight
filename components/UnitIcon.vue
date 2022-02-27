2aabdc837294035bfa70ac2caf051b8a4a47b928
pnly <pnlybubbles@gmail.com>
2022-02-28 04:05:37 +0900

    typoしてた

--- components/UnitIcon.vue
+++ components/UnitIcon.vue
@@ -1,9 +1,9 @@
 <template>
   <font-awesome-icon v-if="unit === 'サニーピース'" icon="sun" />
-  <fone-awesome-icon v-else-if="unit === '月のテンペスト'" icon="moon" />
-  <fone-awesome-icon v-else-if="unit === 'TRINITYAiLE'" icon="feather-pointed" />
-  <fone-awesome-icon v-else-if="unit === 'LizNoir'" icon="mug-hot" />
-  <fone-awesome-icon v-else icon="music" />
+  <font-awesome-icon v-else-if="unit === '月のテンペスト'" icon="moon" />
+  <font-awesome-icon v-else-if="unit === 'TRINITYAiLE'" icon="feather-pointed" />
+  <font-awesome-icon v-else-if="unit === 'LizNoir'" icon="mug-hot" />
+  <font-awesome-icon v-else icon="music" />
 </template>
 <script setup lang="ts">
 interface Props {
