5aeff9b160fc366bac387e659acd24fa84c49558
pnly <pnlybubbles@gmail.com>
2022-03-11 23:37:45 +0900

    アイドル一覧の項目をコンポーネントに切り出し

---
+++ components/IdolItem.vue
@@ -0,0 +1,216 @@
+<template>
+  <div class="idol-item">
+    <div class="heading">
+      <div class="type" :class="idol.type">
+        <RoleIcon :role="idol.role" class="role"></RoleIcon>
+      </div>
+      <div>
+        <span class="title">{{ idol.title }}</span>
+        <span class="name">{{ idol.name }}</span>
+      </div>
+    </div>
+    <div class="status">
+      <div class="skill-overview">
+        <div v-for="skill in idol.skills" :key="skill.id" class="skill-tag">
+          <div class="skill-type">{{ skill.type.toUpperCase() }}</div>
+          <div v-if="skill.type !== 'sp'" class="skill-ct">{{ skill.ct === 0 ? '-' : skill.ct }}</div>
+        </div>
+      </div>
+      <div class="skill-list">
+        <div v-for="skill in idol.skills" :key="skill.id" class="skill-item">
+          <div v-if="skill.type === 'p'" class="ability">
+            <font-awesome-icon icon="flag"></font-awesome-icon>
+            <div>{{ skill.trigger.type }}</div>
+          </div>
+          <div v-for="ability in sortAbility(skill.ability)" :key="ability.id" class="ability">
+            <template v-if="ability.div === 'score'">
+              <RoleIcon role="scorer"></RoleIcon>
+              <div>{{ ability.amount }}%</div>
+            </template>
+            <template v-if="ability.div === 'buff'">
+              <RoleIcon role="buffer"></RoleIcon>
+              <div>{{ ability.amount }}</div>
+              <div>{{ ability.type }}</div>
+              <div>{{ ability.target }}</div>
+              <div v-if="ability.condition">
+                ({{ ability.condition.type }}{{ 'amount' in ability.condition ? ` ${ability.condition.amount}` : '' }})
+              </div>
+              <div>[{{ ability.span }}]</div>
+            </template>
+            <template v-if="ability.div === 'action-buff'">
+              <RoleIcon role="buffer"></RoleIcon>
+              <div>{{ ability.amount }}</div>
+              <div>{{ ability.type }}</div>
+              <div>{{ ability.target }}</div>
+              <div v-if="ability.condition">
+                ({{ ability.condition.type }}{{ 'amount' in ability.condition ? ` ${ability.condition.amount}` : '' }})
+              </div>
+            </template>
+          </div>
+          <div class="ability">
+            <div>lv.{{ skill.level }}</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script setup lang="ts">
+import { AbilityData, AbilityDiv, IdolData, PassiveAbilityData } from '~~/utils/types'
+
+interface Props {
+  idol: IdolData
+}
+defineProps<Props>()
+
+const ABILITY_ORDERING: { [key in AbilityDiv]: number } = {
+  score: 0,
+  buff: 1,
+  'action-buff': 2,
+}
+
+const sortAbility = (ability: AbilityData[] | PassiveAbilityData[]) =>
+  [...ability].sort((a, b) => ABILITY_ORDERING[a.div] - ABILITY_ORDERING[b.div])
+</script>
+<style lang="scss" scoped>
+@import '~~/components/partials/token.scss';
+
+.idol-item {
+  display: grid;
+  grid: auto auto / auto;
+  gap: 8px;
+}
+
+.heading {
+  @include align;
+  display: grid;
+  grid: auto / auto-flow;
+  justify-content: start;
+  align-items: center;
+  gap: 8px;
+}
+
+.title {
+  font-size: $typography-l;
+  font-weight: bold;
+  color: $text1;
+  margin-right: 8px;
+}
+
+.name {
+  font-size: $typography-s;
+  color: $text3;
+}
+
+.role {
+  font-size: 12px;
+  color: $text1;
+}
+
+.type {
+  @include round-corner;
+  height: 20px;
+  width: 20px;
+  display: grid;
+  align-items: center;
+  justify-items: center;
+
+  &.vocal {
+    background-color: $vocal;
+    @include bloom($vocal);
+  }
+
+  &.dance {
+    background-color: $dance;
+    @include bloom($dance);
+  }
+
+  &.visual {
+    background-color: $visual;
+    @include bloom($visual);
+  }
+}
+
+.status {
+  display: grid;
+  grid: auto auto / auto;
+  gap: 10px;
+}
+
+.skill-overview {
+  @include align;
+  display: grid;
+  grid: auto / auto-flow;
+  gap: 8px;
+  justify-content: start;
+}
+
+.skill-tag {
+  @include round-corner;
+  background-color: $surface1;
+  padding: 4px 8px;
+  display: grid;
+  grid: auto / auto-flow;
+  gap: 4px;
+}
+
+.skill-type {
+  font-size: $typography-s;
+  font-weight: bold;
+  color: $text1;
+}
+
+.skill-ct {
+  font-size: $typography-s;
+  font-weight: bold;
+  color: $text3;
+}
+
+.skill-list {
+  display: grid;
+  grid: auto-flow / auto;
+  gap: 8px;
+  overflow-x: auto;
+  width: 100%;
+  padding-bottom: 10px;
+  margin-bottom: -10px;
+}
+
+.skill-item {
+  @include align;
+  display: grid;
+  grid: auto / auto-flow;
+  justify-content: start;
+  gap: 16px;
+}
+
+.ability {
+  display: grid;
+  grid: auto / auto-flow;
+  gap: 4px;
+  font-size: $typography-s;
+  align-items: center;
+  color: $text3;
+  position: relative;
+  white-space: nowrap;
+
+  & + & {
+    &::before {
+      content: '';
+      display: block;
+      position: absolute;
+      left: -8px;
+      top: 50%;
+      width: 2px;
+      height: 2px;
+      border-radius: 50%;
+      transform: translate(-50%, -50%);
+      background-color: $text3;
+    }
+  }
+
+  & svg {
+    font-size: 10px;
+  }
+}
+</style>
--- pages/idol/index.vue
+++ pages/idol/index.vue
@@ -4,68 +4,21 @@
     <div class="main">
       <div v-if="fetching" class="loading"><Spinner></Spinner></div>
       <ul v-else class="list">
-        <li v-for="idol in idolList" :key="idol.id" class="item">
-          <div class="heading">
-            <div class="type" :class="idol.type">
-              <RoleIcon :role="idol.role" class="role"></RoleIcon>
-            </div>
-            <div>
-              <span class="title">{{ idol.title }}</span>
-              <span class="name">{{ idol.name }}</span>
-            </div>
-          </div>
-          <div class="status">
-            <div class="skill-overview">
-              <div v-for="skill in idol.skills" :key="skill.id" class="skill-tag">
-                <div class="skill-type">{{ skill.type.toUpperCase() }}</div>
-                <div v-if="skill.type !== 'sp'" class="skill-ct">{{ skill.ct === 0 ? '-' : skill.ct }}</div>
-              </div>
-            </div>
-            <div class="skill-list">
-              <div v-for="skill in idol.skills" :key="skill.id" class="skill-item">
-                <div v-if="skill.type === 'p'" class="ability">
-                  <font-awesome-icon icon="flag"></font-awesome-icon>
-                  <div>{{ skill.trigger.type }}</div>
-                </div>
-                <div v-for="ability in sortAbility(skill.ability)" :key="ability.id" class="ability">
-                  <template v-if="ability.div === 'score'">
-                    <RoleIcon role="scorer"></RoleIcon>
-                    <div>{{ ability.amount }}%</div>
-                  </template>
-                  <template v-if="ability.div === 'buff'">
-                    <RoleIcon role="buffer"></RoleIcon>
-                    <div>{{ ability.amount }}</div>
-                    <div>{{ ability.type }}</div>
-                    <div>{{ ability.target }}</div>
-                    <div v-if="ability.condition">
-                      ({{ ability.condition.type
-                      }}{{ 'amount' in ability.condition ? ` ${ability.condition.amount}` : '' }})
-                    </div>
-                    <div>[{{ ability.span }}]</div>
-                  </template>
-                  <template v-if="ability.div === 'action-buff'">
-                    <RoleIcon role="buffer"></RoleIcon>
-                    <div>{{ ability.amount }}</div>
-                    <div>{{ ability.type }}</div>
-                    <div>{{ ability.target }}</div>
-                    <div v-if="ability.condition">
-                      ({{ ability.condition.type
-                      }}{{ 'amount' in ability.condition ? ` ${ability.condition.amount}` : '' }})
-                    </div>
-                  </template>
-                </div>
-                <div class="ability">
-                  <div>lv.{{ skill.level }}</div>
-                </div>
-              </div>
-            </div>
-          </div>
+        <li v-for="idol in idolList" :key="idol.id">
+          <button class="item-button" @click="handleClick(idol.id)" @touchend="null">
+            <IdolItem :idol="idol"></IdolItem>
+          </button>
         </li>
       </ul>
-      <div class="button">
+      <div class="add-button">
         <ButtonLink to="/idol/new">アイドルを追加する</ButtonLink>
       </div>
     </div>
+    <Sheet v-model:present="present">
+      <div class="align">
+        <ButtonLink :to="`/idol/${currentIdolId}/edit`">アイドルを編集する</ButtonLink>
+      </div>
+    </Sheet>
   </Layout>
 </template>
 <script setup lang="ts">
@@ -75,7 +28,7 @@ import { useAuth } from '~~/composable/auth0'
 import { useRouteGuard } from '~~/composable/route'
 import { deserializeIdol } from '~~/utils/formatter'
 import { DEFAULT_META } from '~~/utils/meta'
-import { AbilityData, AbilityDiv, PassiveAbilityData } from '~~/utils/types'
+
 const { notAuthenticated } = useAuth()
 const { data, fetching, error } = useQuery({ query: GetIdolListDocument, pause: notAuthenticated })
 if (error.value) {
@@ -83,19 +36,19 @@ if (error.value) {
 }
 const idolList = computed(() => (data.value ? deserializeIdol(data.value) : []))
 
-const ABILITY_ORDERING: { [key in AbilityDiv]: number } = {
-  score: 0,
-  buff: 1,
-  'action-buff': 2,
-}
+const present = ref(false)
+const currentIdolId = ref('')
 
-const sortAbility = (ability: AbilityData[] | PassiveAbilityData[]) =>
-  [...ability].sort((a, b) => ABILITY_ORDERING[a.div] - ABILITY_ORDERING[b.div])
+const handleClick = (idolId: string) => {
+  currentIdolId.value = idolId
+  present.value = true
+}
 
 useRouteGuard()
 useMeta(DEFAULT_META)
 </script>
 <style lang="scss" scoped>
+@import '~~/components/partials/utils.scss';
 @import '~~/components/partials/token.scss';
 
 .main {
@@ -118,146 +71,16 @@ useMeta(DEFAULT_META)
   gap: 16px;
 }
 
-.item {
-  display: grid;
-  grid: auto auto / auto;
-  gap: 8px;
+.item-button {
+  @include clickable;
+  @include reset-button;
 }
 
-.heading {
+.add-button {
   @include align;
-  display: grid;
-  grid: auto / auto-flow;
-  justify-content: start;
-  align-items: center;
-  gap: 8px;
 }
 
-.title {
-  font-size: $typography-l;
-  font-weight: bold;
-  color: $text1;
-  margin-right: 8px;
-}
-
-.name {
-  font-size: $typography-s;
-  color: $text3;
-}
-
-.role {
-  font-size: 12px;
-  color: $text1;
-}
-
-.type {
-  @include round-corner;
-  height: 20px;
-  width: 20px;
-  display: grid;
-  align-items: center;
-  justify-items: center;
-
-  &.vocal {
-    background-color: $vocal;
-    @include bloom($vocal);
-  }
-
-  &.dance {
-    background-color: $dance;
-    @include bloom($dance);
-  }
-
-  &.visual {
-    background-color: $visual;
-    @include bloom($visual);
-  }
-}
-
-.status {
-  display: grid;
-  grid: auto auto / auto;
-  gap: 10px;
-}
-
-.skill-overview {
-  @include align;
-  display: grid;
-  grid: auto / auto-flow;
-  gap: 8px;
-  justify-content: start;
-}
-
-.skill-tag {
-  @include round-corner;
-  background-color: $surface1;
-  padding: 4px 8px;
-  display: grid;
-  grid: auto / auto-flow;
-  gap: 4px;
-}
-
-.skill-type {
-  font-size: $typography-s;
-  font-weight: bold;
-  color: $text1;
-}
-
-.skill-ct {
-  font-size: $typography-s;
-  font-weight: bold;
-  color: $text3;
-}
-
-.skill-list {
-  display: grid;
-  grid: auto-flow / auto;
-  gap: 8px;
-  overflow-x: auto;
-  width: 100%;
-  padding-bottom: 10px;
-  margin-bottom: -10px;
-}
-
-.skill-item {
-  @include align;
-  display: grid;
-  grid: auto / auto-flow;
-  justify-content: start;
-  gap: 16px;
-}
-
-.ability {
-  display: grid;
-  grid: auto / auto-flow;
-  gap: 4px;
-  font-size: $typography-s;
-  align-items: center;
-  color: $text3;
-  position: relative;
-  white-space: nowrap;
-
-  & + & {
-    &::before {
-      content: '';
-      display: block;
-      position: absolute;
-      left: -8px;
-      top: 50%;
-      width: 2px;
-      height: 2px;
-      border-radius: 50%;
-      transform: translate(-50%, -50%);
-      background-color: $text3;
-    }
-  }
-
-  & svg {
-    font-size: 10px;
-  }
-}
-
-.button {
+.align {
   @include align;
 }
 </style>
