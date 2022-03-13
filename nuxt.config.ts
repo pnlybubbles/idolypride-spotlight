84fbd5c78921a60a6e906dee07f51454c074d91f
pnly <pnlybubbles@gmail.com>
2022-03-13 22:29:54 +0900

    なんか動くようになった気がする

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,4 +3,8 @@ import { defineNuxtConfig } from 'nuxt3'
 // https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
 export default defineNuxtConfig({
   css: ['~/global.css'],
+  build: {
+    // なぜか .output/server/node_modules に 特定の exports field のファイルがコピーされてなかったのでトランスパイルすることにした
+    transpile: ['uuid'],
+  },
 })
