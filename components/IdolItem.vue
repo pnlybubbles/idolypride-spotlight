46efc41ec39a95e0c255555af23104e50df2557b
pnly <pnlybubbles@gmail.com>
2022-04-07 03:55:52 +0900

    スキル情報コンポーネントを切り出したのでリファクタ

--- components/IdolItem.vue
+++ components/IdolItem.vue
@@ -17,42 +17,13 @@
         </div>
       </div>
       <div v-if="variant === 'default'" class="skill-list">
-        <div v-for="skill in idol.skills" :key="skill.id" class="skill-item">
-          <div v-for="ability in sortAbility(skill.ability)" :key="ability.id" class="ability">
-            <template v-if="ability.div === 'score'">
-              <RoleIcon role="scorer"></RoleIcon>
-              <div>{{ ability.amount }}%</div>
-            </template>
-            <template v-if="ability.div === 'buff'">
-              <RoleIcon role="buffer"></RoleIcon>
-              <div>{{ ability.amount }}</div>
-              <div>{{ ability.type }}</div>
-              <div>{{ ability.target }}</div>
-              <div v-if="ability.condition.type !== 'none'">
-                ({{ ability.condition.type }}{{ 'amount' in ability.condition ? ` ${ability.condition.amount}` : '' }})
-              </div>
-              <div>[{{ ability.span }}]</div>
-            </template>
-            <template v-if="ability.div === 'action-buff'">
-              <RoleIcon role="buffer"></RoleIcon>
-              <div>{{ ability.amount }}</div>
-              <div>{{ ability.type }}</div>
-              <div>{{ ability.target }}</div>
-              <div v-if="ability.condition.type !== 'none'">
-                ({{ ability.condition.type }}{{ 'amount' in ability.condition ? ` ${ability.condition.amount}` : '' }})
-              </div>
-            </template>
-          </div>
-          <div class="ability">
-            <div>lv.{{ skill.level }}</div>
-          </div>
-        </div>
+        <SkillText v-for="skill in idol.skills" :key="skill.id" :skill="skill" class="skill-item"></SkillText>
       </div>
     </div>
   </div>
 </template>
 <script setup lang="ts">
-import { AbilityData, AbilityDiv, IdolData, PassiveAbilityData } from '~~/utils/types'
+import { IdolData } from '~~/utils/types'
 
 interface Props {
   idol: IdolData
@@ -64,15 +35,6 @@ interface Emits {
   (e: 'click'): void
 }
 defineEmits<Emits>()
-
-const ABILITY_ORDERING: { [key in AbilityDiv]: number } = {
-  score: 0,
-  buff: 1,
-  'action-buff': 2,
-}
-
-const sortAbility = (ability: AbilityData[] | PassiveAbilityData[]) =>
-  [...ability].sort((a, b) => ABILITY_ORDERING[a.div] - ABILITY_ORDERING[b.div])
 </script>
 <style lang="scss" scoped>
 @import '~~/components/partials/token.scss';
@@ -183,39 +145,6 @@ const sortAbility = (ability: AbilityData[] | PassiveAbilityData[]) =>
 
 .skill-item {
   @include align;
-  display: grid;
-  grid: auto / auto-flow;
-  justify-content: start;
-  gap: 16px;
-}
-
-.ability {
-  display: grid;
-  grid: auto / auto-flow;
-  gap: 4px;
-  font-size: $typography-s;
-  align-items: center;
   color: $text3;
-  position: relative;
-  white-space: nowrap;
-
-  & + & {
-    &::before {
-      content: '';
-      display: block;
-      position: absolute;
-      left: -8px;
-      top: 50%;
-      width: 2px;
-      height: 2px;
-      border-radius: 50%;
-      transform: translate(-50%, -50%);
-      background-color: $text3;
-    }
-  }
-
-  & svg {
-    font-size: 10px;
-  }
 }
 </style>
