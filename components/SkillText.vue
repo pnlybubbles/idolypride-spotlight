3ab80cf619926914bafd57a247e1e807a710a72e
pnly <pnlybubbles@gmail.com>
2022-04-25 01:19:53 +0900

    スコア獲得スキルの条件を表示

--- components/SkillText.vue
+++ components/SkillText.vue
@@ -5,6 +5,9 @@
       <template v-if="ability.div === 'score'">
         <RoleIcon role="scorer"></RoleIcon>
         <div>{{ ability.amount }}%</div>
+        <div v-if="ability.condition.type !== 'none'">
+          ({{ ability.condition.type }}{{ 'amount' in ability.condition ? ` ${ability.condition.amount}` : '' }})
+        </div>
       </template>
       <template v-if="ability.div === 'buff'">
         <RoleIcon role="buffer"></RoleIcon>
