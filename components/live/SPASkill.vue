2c4fa3abbee88f54881b7b27d8934d6db3358fe1
pnly <pnlybubbles@gmail.com>
2022-04-26 02:01:46 +0900

    A,SPスキルマーカのクリック領域をサークルだけに限定

--- components/live/SPASkill.vue
+++ components/live/SPASkill.vue
@@ -1,18 +1,16 @@
 <template>
-  <Interactive
-    class="skill"
-    :class="{ fail, [variant]: true }"
-    @long-press="$emit('long-press')"
-    @click="showTooltip = !showTooltip"
-  >
-    <div class="beat">{{ beat }}</div>
+  <div class="skill">
+    <Interactive class="hit" @long-press="$emit('long-press')" @click="showTooltip = !showTooltip">
+      <div class="marker" :class="{ fail, [variant]: true }"></div>
+    </Interactive>
+    <div class="beat" :class="variant">{{ beat }}</div>
     <LiveTooltip
       v-show="showTooltip"
       :activated="activated"
       :skill="skill"
       :position="lane === 0 ? 'right' : 'left'"
     ></LiveTooltip>
-  </Interactive>
+  </div>
 </template>
 <script setup lang="ts">
 import { AbilityType, BuffAbilityType, Lane, SkillData } from '~~/utils/types'
@@ -43,30 +41,63 @@ const color = computed(() => cssBuff(buff.value))
 const showTooltip = ref(false)
 </script>
 <style lang="scss" scoped>
-@import './skill.scss';
 @import '~~/components/partials/token.scss';
 
 .skill {
+  position: absolute;
+  top: calc(v-bind(top));
+  left: 50%;
+}
+
+.hit {
+  position: absolute;
+  width: 24px;
+  height: 24px;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+}
+
+$sp-size: 40px;
+$a-size: 20px;
+
+.marker {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  border: 2px solid v-bind(color);
+  border-radius: 100%;
+
+  &.sp {
+    width: $sp-size;
+    height: $sp-size;
+  }
+
+  &.a {
+    width: $a-size;
+    height: $a-size;
+  }
+
   &.fail {
     border-color: red;
     background: repeating-linear-gradient(45deg, red, red 2px, transparent 2px, transparent 6px);
   }
 }
 
-.sp {
-  @include skill(40px, v-bind(color), v-bind(top));
-}
-
-.a {
-  @include skill(20px, v-bind(color), v-bind(top));
-}
-
 .beat {
   top: 50%;
-  right: calc(100% + 6px);
   transform: translateY(-50%);
   position: absolute;
   font-size: $typography-m;
   color: $text3;
+
+  &.sp {
+    right: calc(100% + 4px + $sp-size / 2);
+  }
+
+  &.a {
+    right: calc(100% + 4px + $a-size / 2);
+  }
 }
 </style>
