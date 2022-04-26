ea009bfefaf880b8d1895ff3218d0ea109cb602d
pnly <pnlybubbles@gmail.com>
2022-04-27 02:38:47 +0900

    ラベルを導出する関数をcommonに移動

--- components/SkillText.vue
+++ components/SkillText.vue
@@ -5,19 +5,20 @@
       <template v-if="ability.div === 'score'">
         <RoleIcon role="scorer"></RoleIcon>
         <div>{{ ability.amount }}%</div>
-        <div v-if="ability.enhance.type !== 'none'">
-          {{ enhanceTypeLabel(ability.enhance.type)
-          }}{{ 'value' in ability.enhance ? ` ${ability.enhance.value}` : '' }}
+        <div v-if="ability.enhance.type !== 'none'">{{ abilityEnhanceLabel(ability.enhance, internalLabel) }}</div>
+        <div v-if="ability.condition.type !== 'none'">
+          ({{ abilityConditionTypeLabel(ability.condition, internalLabel) }})
         </div>
-        <div v-if="ability.condition.type !== 'none'">({{ abilityConditionTypeLabel(ability.condition) }})</div>
       </template>
       <template v-if="ability.div === 'buff' || ability.div === 'action-buff'">
         <RoleIcon role="buffer"></RoleIcon>
         <div v-if="!deriveDisabledAmount(ability.type)">{{ ability.amount }}</div>
-        <div v-if="ability.div === 'buff'">{{ buffAbilityTypeLabel(ability.type) }}</div>
-        <div v-else-if="ability.div === 'action-buff'">{{ actionAbilityTypeLabel(ability.type) }}</div>
-        <div>{{ abilityTargetLabel(ability.target) }}</div>
-        <div v-if="ability.condition.type !== 'none'">({{ abilityConditionTypeLabel(ability.condition) }})</div>
+        <div v-if="ability.div === 'buff'">{{ buffAbilityTypeLabel(ability.type, internalLabel) }}</div>
+        <div v-else-if="ability.div === 'action-buff'">{{ actionAbilityTypeLabel(ability.type, internalLabel) }}</div>
+        <div>{{ abilityTargetLabel(ability.target, internalLabel) }}</div>
+        <div v-if="ability.condition.type !== 'none'">
+          ({{ abilityConditionTypeLabel(ability.condition, internalLabel) }})
+        </div>
         <div v-if="ability.div === 'buff'">[{{ ability.span }}]</div>
       </template>
     </div>
@@ -28,28 +29,15 @@
 </template>
 <script setup lang="ts">
 import { useInternalLabel } from '~~/composable/localstorage-descriptors'
+import { AbilityData, AbilityDiv, PassiveAbilityData, SkillData } from '~~/utils/types'
+import { deriveDisabledAmount } from './idol-form/helper'
 import {
-  ABILITY_CONDITION_WITHOUT_VALUE,
-  ABILITY_CONDITION_WITH_VALUE,
-  ABILITY_ENHANCE,
-  ACTION_ABILITY_TYPE,
-  BUFF_ABILITY_TYPE,
-  BUFF_TARGET_WITHOUT_SUFFIX,
-  BUFF_TARGET_WITH_SUFFIX,
-  isBuffTargetWithoutSuffix,
-} from '~~/utils/formatter'
-import {
-  AbilityCondition,
-  AbilityData,
-  AbilityDiv,
-  AbilityEnhanceType,
-  ActionAbilityType,
-  BuffAbilityType,
-  PassiveAbilityData,
-  PassiveBuffTarget,
-  SkillData,
-} from '~~/utils/types'
-import { extractBuffTarget, deriveDisabledAmount } from './idol-form/helper'
+  abilityEnhanceLabel,
+  abilityConditionTypeLabel,
+  buffAbilityTypeLabel,
+  actionAbilityTypeLabel,
+  abilityTargetLabel,
+} from '~~/utils/common'
 
 interface Props {
   skill: SkillData
@@ -67,31 +55,6 @@ const sortAbility = (ability: AbilityData[] | PassiveAbilityData[]) =>
   [...ability].sort((a, b) => ABILITY_ORDERING[a.div] - ABILITY_ORDERING[b.div])
 
 const [internalLabel] = useInternalLabel()
-
-const enhanceTypeLabel = (type: AbilityEnhanceType) => (internalLabel.value ? type : ABILITY_ENHANCE[type])
-const buffAbilityTypeLabel = (type: BuffAbilityType) => (internalLabel.value ? type : BUFF_ABILITY_TYPE[type])
-const actionAbilityTypeLabel = (type: ActionAbilityType) => (internalLabel.value ? type : ACTION_ABILITY_TYPE[type])
-// TODO: 専用のマッピングが合ったほうが良い気がする
-const abilityTargetLabel = (t: PassiveBuffTarget) => {
-  if (internalLabel.value) {
-    return t
-  }
-  const { target, targetSuffix } = extractBuffTarget(t)
-  if (isBuffTargetWithoutSuffix(target)) {
-    if (target === 'triggered') {
-      return '発動対象'
-    }
-    return BUFF_TARGET_WITHOUT_SUFFIX[target]
-  } else {
-    return BUFF_TARGET_WITH_SUFFIX[target].replace(/X/, targetSuffix)
-  }
-}
-const abilityConditionTypeLabel = (condition: AbilityCondition) =>
-  internalLabel.value
-    ? `${condition.type}${'amount' in condition ? ` ${condition.amount}` : ''}`
-    : 'amount' in condition
-    ? ABILITY_CONDITION_WITH_VALUE[condition.type].replace(/X/, condition.amount.toString())
-    : ABILITY_CONDITION_WITHOUT_VALUE[condition.type]
 </script>
 <style lang="scss" scoped>
 @import '~~/components/partials/token.scss';
