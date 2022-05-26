15354cea0546f74a1281557dd05c2bcfdad250c9
pnly <pnlybubbles@gmail.com>
2022-05-27 01:34:54 +0900

    graphqlのエンドポイントも環境変数を優先するように

--- codegen.config.js
+++ codegen.config.js
@@ -1,9 +1,14 @@
 /* global module,process */
 /** @type { import('@graphql-codegen/plugin-helpers').Types.Config } */
+
+if (process.env.HASURA_GRAPHQL_ADMIN_SECRET == null) {
+  throw new Error('environment variable `HASURA_GRAPHQL_ADMIN_SECRET` is not defined.')
+}
+
 module.exports = {
   overwrite: true,
   schema: {
-    'https://idolypride-spotlight.hasura.app/v1/graphql': {
+    [`${process.env.HASURA_GRAPHQL_ENDPOINT ?? 'https://idolypride-spotlight.hasura.app'}/v1/graphql`]: {
       headers: {
         'x-hasura-admin-secret': process.env.HASURA_GRAPHQL_ADMIN_SECRET,
         'x-hasura-role': 'user',
